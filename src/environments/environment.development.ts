9e952ed2956189498abe81834740747720bf25bb
angelogabrielrocha <angelogabrielrocha27@gmail.com>
2023-04-20 10:55:06 -0300

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyDc4Lm71iEIetWF_rZPO9r9GazFHgJH1ow",
+      authDomain: "aplicativo-kanban.firebaseapp.com",
+      databaseURL:  "https://aplicativo-kanban-default-rtdb.firebaseio.com",
+      projectId: "aplicativo-kanban",
+      storageBucket: "aplicativo-kanban.appspot.com",
+      messagingSenderId: "382458260506",
+    }
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDc4Lm71iEIetWF_rZPO9r9GazFHgJH1ow",
+    authDomain: "aplicativo-kanban.firebaseapp.com",
+    databaseURL: "https://aplicativo-kanban-default-rtdb.firebaseio.com",
+    projectId: "aplicativo-kanban",
+    storageBucket: "aplicativo-kanban.appspot.com",
+    messagingSenderId: "382458260506",
+    appId: "1:382458260506:web:92eed66bb886a8dccc35a3",
+    measurementId: "G-VNBRETZ7GS"
+  };
+  export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyDc4Lm71iEIetWF_rZPO9r9GazFHgJH1ow",
+      authDomain: "aplicativo-kanban.firebaseapp.com",
+      databaseURL:  "https://aplicativo-kanban-default-rtdb.firebaseio.com",
+      projectId: "aplicativo-kanban",
+      storageBucket: "aplicativo-kanban.appspot.com",
+      messagingSenderId: "382458260506",
+    }
+  };
